9a8a5d451394cb1fef20923cb060d3179af393de
ArminRodriguez1 <44068727+ArminRodriguez1@users.noreply.github.com>
2020-11-21 00:03:14 -0300

    Add files via upload

---
+++ Excepcion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author arodr
+ */
+public class Excepcion extends Exception{
+    public Excepcion(String mensaje){
+        super(mensaje);
+    }
+}
